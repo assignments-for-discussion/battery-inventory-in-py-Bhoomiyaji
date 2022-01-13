11f00065ce1724168d561cfae0f0f45387521ea3
Bhoomiyaji <97046560+Bhoomiyaji@users.noreply.github.com>
2022-01-13 18:52:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-
 def count_batteries_by_usage(cycles):
-  return {
-    "lowCount": 0,
-    "mediumCount": 0,
-    "highCount": 0
-  }
+    hashmap = {"lowCount": 0, "mediumCount": 0, "highCount": 0}
+    for c in cycles:
+        if c < 400:
+            hashmap["lowCount"] +=1
+        elif 400 <= c <= 919:
+            hashmap["mediumCount"] += 1
+        elif c >= 920:
+            hashmap["highCount"] += 1
+
+    return hashmap
 
 
 def test_bucketing_by_number_of_cycles():
-  print("Counting batteries by usage cycles...\n");
-  counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
-  assert(counts["lowCount"] == 2)
-  assert(counts["mediumCount"] == 3)
-  assert(counts["highCount"] == 1)
-  print("Done counting :)")
+    print("Counting batteries by usage cycles...\n")
+    counts = count_batteries_by_usage([100, 300, 400, 500, 600, 900, 920, 1000])
+    assert(counts["lowCount"] == 2)
+    assert(counts["mediumCount"] == 4)
+    assert(counts["highCount"] == 2)
+    print("Done counting: ")
 
 
-if __name__ == '__main__':
-  test_bucketing_by_number_of_cycles()
+if __name__ == "__main__":
+    test_bucketing_by_number_of_cycles()
